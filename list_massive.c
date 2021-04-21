62c90da7754a3d92abada85a189059daf5b8e023
niciki <71276784+niciki@users.noreply.github.com>
2021-04-22 01:06:10 +0300

    Реализация списка с использованием массива

--- list_massive.c
+++ list_massive.c
@@ -1,7 +1,28 @@
+//list.h
+#ifndef list_h
+#define list_h
 #include <stdlib.h>
 #include <stdio.h>
-#include "list.h"
 
+typedef struct list{
+    int number_of_elements;
+    int capacity;
+    char *elements;
+}list;
+
+void list_create(list *l,int capacity);
+int is_list_empty(list *l);
+int size(list *l);
+void resize(list *l);
+void push_back(list *l, char c);
+void list_print(list *l);
+void delete_element_of_list(list *l);
+void list_add_index(list *l);
+int number_of_elements_on_lists(list *l);
+void reverse(list *l);
+#endif
+
+//list.c
 typedef struct list{
     int number_of_elements;
     int capacity;
